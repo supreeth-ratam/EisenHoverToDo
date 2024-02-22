f79ebdac69c58e0cccf4a0c27cf4cc6a58d3d4a9
supreeth <supreethratam@gmail.com>
2024-02-22 21:53:31 +0530

    added firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      
+      <h1>This is a heading</h1>
+      <p>This is another paragraph</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
